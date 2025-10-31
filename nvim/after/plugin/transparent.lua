63fa6297e60dc70e66c61839a9902e8d5491d6e0
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2025-11-01 04:52:24 +0530

    new lazyvim

---
+++ nvim/after/plugin/transparent.lua
@@ -0,0 +1,11 @@
+vim.cmd([[
+  hi Normal guibg=none ctermbg=none
+  hi NormalNC guibg=none ctermbg=none
+  hi NormalFloat guibg=none ctermbg=none
+  hi SignColumn guibg=none ctermbg=none
+  hi VertSplit guibg=none ctermbg=none
+  hi StatusLine guibg=none ctermbg=none
+  hi StatusLineNC guibg=none ctermbg=none
+  hi EndOfBuffer guibg=none ctermbg=none
+]])
+
