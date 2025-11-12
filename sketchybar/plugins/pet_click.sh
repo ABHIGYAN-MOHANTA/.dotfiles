c8564ad913c90572e6c7fd9045b2cbd2db0ded1b
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2025-11-12 16:42:41 +0530

    sketchybar v2

---
+++ sketchybar/plugins/pet_click.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Change cat to heart for 1 second, then back to cat
+sketchybar --set pet icon= icon.y_offset=0 icon.color=0xFF98D8ED icon.font="Hack Nerd Font:Bold:18.0"
+sleep 1
+sketchybar --set pet icon=󱃾 icon.y_offset=1.5 icon.color=0xFF7EC8E3 icon.font="Hack Nerd Font:Bold:18.0"
