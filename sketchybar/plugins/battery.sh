cc5af97d9f1bdf9990928b3b726645db17fe8ead
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2025-11-12 16:59:09 +0530

    Add more battery icons to battery.sh

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -9,7 +9,7 @@ fi
 
 ICON=""
 
-#     󰕈   󰟓    󱀏        
+#     󰕈   󰟓    󱀏                   
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
 sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%" icon.font="Hack Nerd Font:Bold:40.0" icon.color=0xFF98D8ED
