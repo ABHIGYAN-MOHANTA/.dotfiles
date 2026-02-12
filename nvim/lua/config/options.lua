23341fab759b3b3068189c735ec981d1d011a766
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2026-02-12 20:56:14 +0530

    fix indents

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.tabstop = 2 -- number of visual spaces per TAB
+opt.softtabstop = 2 -- number of spaces in tab when editing
+opt.shiftwidth = 2 -- number of spaces for autoindent
+opt.expandtab = true -- tabs are converted to spaces
